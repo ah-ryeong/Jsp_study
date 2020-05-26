f8e7227011cd584d520d64a052039c841757788b
unknown <aryeong22@hanail.net>
2020-05-26 16:33:57 +0900

    200526

---
+++ Chapter3/src/MemRegServlet.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/memReg")
+public class MemRegServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public MemRegServlet() {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=euc-kr");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		String addr = request.getParameter("addr");
+		String tel = request.getParameter("tel");
+		String hobby = request.getParameter("hobby");
+		out.println("이름 = " + name + "<br>");
+		out.println("주소 = " + name + "<br>");
+		out.println("전화번호 = " + name + "<br>");
+		out.println("취미 = " + name + "<br>");
+	}
+
+}
